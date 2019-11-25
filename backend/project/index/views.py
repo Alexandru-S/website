b5866d9ea6ee736d947bd9e650b2338589250b62
Alexandru-S <alex.sulea@gmail.com>
2019-11-25 21:21:59 +0000

    file directory fix

--- backend/project/index/views.py
+++ backend/project/index/views.py
@@ -3,7 +3,7 @@ from flask import render_template, Blueprint, request, flash, redirect, url_for
 index_blueprint = Blueprint('index', __name__, template_folder='templates')
 
 
-@index_blueprint.route('/index', methods=['GET', 'POST'])   # pragma: no cover
+@index_blueprint.route('/', methods=['GET', 'POST'])   # pragma: no cover
 def index():
     if request.method == 'GET':
         print("get meyhod here")
