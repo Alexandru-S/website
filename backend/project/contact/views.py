e6f68b7faa801ce9e2d63a0ed5ba3697ceeac519
Alexandru-S <alex.sulea@gmail.com>
2019-12-03 00:06:21 +0000

    added comments and fixed code syntax

--- backend/project/contact/views.py
+++ backend/project/contact/views.py
@@ -1,9 +1,16 @@
-from flask import render_template, Blueprint, request, flash, redirect, url_for
+from flask import render_template, Blueprint, request
 
 contact_blueprint = Blueprint('contact', __name__, template_folder='templates')
 
 
-@contact_blueprint.route('/contact', methods=['GET', 'POST'])   # pragma: no cover
+@contact_blueprint.route('/contact', methods=['GET', 'POST'])
 def contact():
+    """ Returns the HTML for contacts page.
+    Args:
+      none: takes no args for now.
+
+    Returns:
+      contact.html file if request is GET
+    """
     if request.method == 'GET':
         return render_template('contact.html')
