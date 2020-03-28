912ace568770610b6a136c20c5714a9e9eeec8d3
alex sulea <alexsulea@alexs-MacBook-Pro.local>
2020-03-28 23:55:36 +0000

    added funtion commentes

--- mywebsite/blog/views.py
+++ mywebsite/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from  django.http import HttpResponse
 
+# function to render homepage template
 def home(request):
     return render(request, 'blog/home.html')
-
+# function to render about template
 def about(request):
     return render(request, 'blog/about.html')
