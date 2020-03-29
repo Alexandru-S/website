5a9c3683a223a3d0673892f4e7d6cb889b7f1fa1
alex sulea <alexsulea@alexs-MacBook-Pro.local>
2020-03-29 23:34:33 +0100

    added templating

--- mywebsite/blog/views.py
+++ mywebsite/blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from  django.http import HttpResponse
+from django.http import HttpResponse
 
+posts = [
+
+    {
+        'author': 'AlexMS',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': '2020-03-30'
+    },
+    {
+        'author': 'RazvanMS',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': '2020-03-31'
+    }
+
+
+]
 # function to render homepage template
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {'posts':posts,'title':'Home'}
+    return render(request, 'blog/home.html', context)
+
+
 # function to render about template
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title':'About'})
