c27f6d762a4501c3f4ca7eaad7d26072d23cae10
alex sulea <alexsulea@alexs-MacBook-Pro.local>
2020-04-01 23:49:24 +0100

    lkinked backend tyo blogs

--- mywebsite/blog/views.py
+++ mywebsite/blog/views.py
@@ -1,30 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-
-    {
-        'author': 'AlexMS',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': '2020-03-30'
-    },
-    {
-        'author': 'RazvanMS',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': '2020-03-31'
-    }
+from .models import Post
 
 
-]
-
-# function to render homepage template
 def home(request):
-    context = {'posts':posts,'title':'Home'}
+    context = {
+        'posts': Post.objects.all()
+    }
     return render(request, 'blog/home.html', context)
 
 
-# function to render about template
 def about(request):
-    return render(request, 'blog/about.html', {'title':'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
